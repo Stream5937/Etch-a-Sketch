f65904d5f5446baffdd8cd2871d63fd7f5bef469
Stream5937 <149094480+Stream5937@users.noreply.github.com>
2024-01-03 20:27:36 +0000

    add grid size selector button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,42 +1,52 @@
-//the main continer
+//the main container
 const sketch = document.querySelector('.sketch');
 let rowContainer;
 let selectedCell;
 
-for(let j=0; j<16; j++){
-    //each row container
-    rowContainer= document.createElement('div');
-    rowContainer.classList.add('rowContainer');
-    sketch.appendChild(rowContainer);
 
-    for(let i=0; i<16; i++){
-        const div = document.createElement('div');
-        div.classList.add('gridDiv');
-        rowContainer.appendChild(div);
-    }
+sketch.onmouseover = (event) => {
+  let e = event.target.closest('div'); // (1)
+  if (!e) return;
+  if (e.tagName != 'DIV') return;
+  highlight(e);
+};
+
+function highlight(cell) {
+  /*
+  if (selectedCell) { // remove the existing highlight if any
+    selectedCell.classList.remove('highlight');
+  }
+  */
+  selectedCell = cell;
+  selectedCell.classList.add('highlight'); // highlight the new cell  
 }
 
-sketch.onmouseover = function(event) {
-    let e = event.target.closest('div'); // (1)
-    //console.log("at e");
-    if (!e) return; // (2)
-    //console.log("at past !e");
-    console.log("e tagname = " + e.tagName);
-    if (e.tagName != 'DIV') return;
-    //console.log("at past !div");
-    highlight(e); // (4)
-    //console.log("returned from highlight");
-  };
 
-  function highlight(cell) {
-    //console.log("At function call cell = " +cell);
-    /*
-    if (selectedCell) { // remove the existing highlight if any
-      selectedCell.classList.remove('highlight');
-    }
-    */
-    selectedCell = cell;
-    console.log("selected cell = " + selectedCell)
-    selectedCell.classList.add('highlight'); // highlight the new cell
+
+function gridSetUp(){
+  let numStr= prompt("Input number of grids/side? -max 100 ");
+  let num = parseInt(numStr);
+  if(num > 100){
+    num = 100;
   }
+  let maxSketchWidth = 800;
+  let gridSize = Math.round(maxSketchWidth/num);
+  console.log("grid size = "+ gridSize);
+  console.log("as String "+(gridSize.toString()+'px'));
+  for(let j=0; j< num; j++){
+        //each row container
+        rowContainer= document.createElement('div');
+        rowContainer.classList.add('rowContainer');
+        rowContainer.style.width= '800px';
+        rowContainer.style.height= (gridSize.toString()+'px');
+        sketch.appendChild(rowContainer);
 
+        for(let i= 0; i< num; i++){
+            element = document.createElement('div');
+            element.classList.add('gridDiv');
+            element.style.width= (gridSize.toString()+'px');
+            element.style.height= (gridSize.toString()+'px');
+            rowContainer.appendChild(element);
+        }
+    }
+}
